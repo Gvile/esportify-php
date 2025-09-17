f19caf8c1e0cb58a3dca54ac6dcfe6630e9502c1
Gvile <guillaumevillemart@gmail.com>
2025-09-17 17:51:14 +0200

    Enable intl extension on Heroku

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+
+        $user->setEmail("Admin@gmail.com")->setPassword($this->passwordHasher->hashPassword($user, "Azerty12345"))->setRoles(['ROLE_ADMIN'])->setPseudo("Guillaume");
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
